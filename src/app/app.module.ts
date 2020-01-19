0a3bc36068b265a5e079a965b235848960737947
田中龍男 <taexa0501@gmail.com>
2020-01-19 22:39:10 +0900

    ヘッダーコンポーネント変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NavbarComponent } from './common/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
